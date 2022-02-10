9cf14d6f772da6f6c7323fff7f2bb8bacd9edf7d
Tinku István <tinkuistvan@Tinku-MacBook-Pro.local>
2022-02-10 18:02:24 +0100

    feat: Add and animate bell

---
+++ Bankey/Bankey/Components/ShakeyBellView.swift
@@ -0,0 +1,90 @@
+//
+//  ShakeyBellView.swift
+//  Bankey
+//
+//  Created by Tinku István on 2022. 02. 09..
+//
+
+import UIKit
+
+class ShakeyBellView: UIView {
+    
+    let imageView = UIImageView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+        style()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 48, height: 48)
+    }
+    
+}
+
+extension ShakeyBellView {
+    
+    func setup() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(bellTapped(_:)))
+        imageView.addGestureRecognizer(tapGesture)
+        imageView.isUserInteractionEnabled = true
+    }
+    
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        let image = UIImage(systemName: "bell.fill")!.withTintColor(.white, renderingMode: .alwaysOriginal)
+        imageView.image = image
+    }
+    
+    func layout() {
+        addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalToConstant: 24),
+            imageView.heightAnchor.constraint(equalToConstant: 24),
+            imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+}
+
+//MARK: - Actions
+extension ShakeyBellView {
+    @objc func bellTapped(_ recognizer: UITapGestureRecognizer) {
+        shakeWith(duration: 1.0, angle: .pi/8)
+    }
+    
+    private func shakeWith(duration: Double, angle: CGFloat) {
+        
+        let numberOfFrames : Double = 6
+        let frameDuration = 1 / numberOfFrames
+        
+        UIView.animateKeyframes(withDuration: duration, delay: 0, options: [], animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0.0, relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: -angle)
+            }
+            UIView.addKeyframe(withRelativeStartTime: frameDuration, relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: +angle)
+            }
+            UIView.addKeyframe(withRelativeStartTime: frameDuration * 2, relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: -angle)
+            }
+            UIView.addKeyframe(withRelativeStartTime: frameDuration * 3, relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: +angle)
+            }
+            UIView.addKeyframe(withRelativeStartTime: frameDuration * 4, relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform(rotationAngle: -angle)
+            }
+            UIView.addKeyframe(withRelativeStartTime: frameDuration * 5, relativeDuration: frameDuration) {
+                self.imageView.transform = CGAffineTransform.identity //original position
+            }
+        }, completion: nil)
+
+    }
+}
